3aac3426d072a0639963a77b2efd9e74ba286fe3
fardisaz <amoozadeh73@yahoo.com>
2021-01-21 10:59:56 +0100

    first-commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,90 @@
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+<div class="pagination">
+	<router-link
+	id="page-prev"
+		:to="{name:'EventList',query:{page:page-1}}"
+		rel="prev"
+		v-if="page!=1">&#60; Previous</router-link>
+		<router-link
+		id="page-next"
+		:to="{name:'EventList',query:{page:page+1}}"
+		rel="next"
+		v-if="hasNextPage"
+		>Next &#62;</router-link>
+		</div>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+import {watchEffect} from 'vue'
+
+export default {
+  name: 'EventList',
+  props:['page'],
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+	  events: null,
+	  totalEvents:0
+    }
+  },
+  created() {
+	  //when reactive objects that are accessed inside this function change
+	  //vue willrun this function again
+	  watchEffect(()=>{
+		  //this will clear out the events on the page,
+		  //so our user knows the API has been called
+		  this.events=null
+		  //accessing page which is reactive
+    EventService.getEvents(2,this.page)
+      .then(response => {
+		this.events = response.data
+		//only shows nextPage link when there is a next page
+		this.totalEvents=response.headers['x-total-count']
+      })
+      .catch(error => {
+        console.log(error)
+	  })
+	  
+	  })
+  },
+  computed:{
+	  hasNextPage(){
+		  var totalPages=Math.ceil(this.totalEvents/2)
+		  return this.page<totalPages
+	  }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+</style>
